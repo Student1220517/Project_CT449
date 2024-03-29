2cd486befbc575e16b8fb4eace6ca8bce0b66629
Truong Minh Tri <truongminhtri1905@gmail.com>
2024-03-29 13:56:13 +0700

    update route

--- Quanlymuonsach-frontend/src/route/index.js
+++ Quanlymuonsach-frontend/src/route/index.js
@@ -1,7 +1,12 @@
 import { createWebHistory, createRouter } from "vue-router";
-import ContactBook  from "@/view/ContactBook.vue";
+import ContactBook from "@/view/ContactBook.vue";
 
 const routes = [
+    {
+        path: "/",
+        name: "contactbook",
+        component: ContactBook,
+    },
     {
         path: "/:pathMatch(.*)*",
         name: "notfound",
